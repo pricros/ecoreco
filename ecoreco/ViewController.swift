0cd2a917ae3132bfcaa91b208a5474b10b059bcd
Jacky Lin <pricros@gmail.com>
2015-12-21 12:44:40 +0800

    view 1 basic layout ok

--- ecoreco/ViewController.swift
+++ ecoreco/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var headerImage: UIImageView!
+    @IBOutlet weak var lockImage: UIImageView!
+    @IBOutlet weak var partitionImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let image1  = UIImage(named: "p1")
+        let imageLock  = UIImage(named: "lock")
+        let imageHeader  = UIImage(named: "header")
+        partitionImage.image = image1
+        lockImage.image = imageLock
+        headerImage.image = imageHeader
     }
 
     override func didReceiveMemoryWarning() {
