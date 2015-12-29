b713141ff127ed16be0665df283799b454a0ac11
Jacky Lin <pricros@gmail.com>
2015-12-29 12:15:49 +0800

    page 1, 4 ok, except BG RGB value

--- ecoreco/ViewController.swift
+++ ecoreco/ViewController.swift
@@ -12,16 +12,49 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var headerImage: UIImageView!
 
+    @IBOutlet weak var imageDigit1: UIButton!
+    @IBOutlet weak var imageDigit3: UIButton!
+    @IBOutlet weak var imageDigit2: UIButton!
+    @IBOutlet weak var imageDigit4: UIButton!
+    @IBOutlet weak var imageDigit5: UIButton!
+    @IBOutlet weak var imageDigit6: UIButton!
+    @IBOutlet weak var imageDigit7: UIButton!
+    @IBOutlet weak var imageDigit8: UIButton!
+    @IBOutlet weak var imageDigit9: UIButton!
+    @IBOutlet weak var imageDigit0: UIButton!
+    @IBOutlet weak var imageClear: UIButton!
+    
+    var digitImages:[UIButton] = [UIButton]()
+    
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-
         let imageHeader  = UIImage(named: "header")
-
+        var fileName:String
+        self.digitImages = [self.imageDigit0,self.imageDigit1,self.imageDigit2,self.imageDigit3,self.imageDigit4,self.imageDigit5,self.imageDigit6,self.imageDigit7,self.imageDigit8,self.imageDigit9,self.imageClear]
+        
+ 
         headerImage.image = imageHeader
+        
+        for index in 0...10 {
+            if (index < 10)
+            {
+                fileName = "digit_\(index)"
+            }
+            else
+            {
+                fileName = "digit_clear"
+            }
+            
+            let image_digit = UIImage(named: fileName)! as UIImage
+            digitImages[index].setBackgroundImage(image_digit, forState: .Normal)
+            digitImages[index].setTitle("", forState: .Normal)
+        }
+
+        
     }
 
     override func didReceiveMemoryWarning() {
