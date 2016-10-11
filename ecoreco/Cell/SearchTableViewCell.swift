707aa05179916f359146a17df357a153895db0a6
Jacky Lin <jacky@huanuage.com>
2016-10-11 22:03:09 +0800

    draw radar

---
+++ ecoreco/Cell/SearchTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchTableViewCell.swift
+//  ecoreco
+//
+//  Created by admin on 2016/10/11.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lableName: UILabel!
+    @IBOutlet weak var bgImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ecoreco/PairViewController.swift
+++ ecoreco/PairViewController.swift
@@ -6,12 +6,22 @@
 //  Copyright © 2016年 apple. All rights reserved.
 //
 
+import UIKit
 import Foundation
 
 class PairViewController: CommonViewController {
+    @IBOutlet weak var labelSearch: UILabel!
+    
+    @IBOutlet weak var radarView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        labelSearch!.textColor = UIColor.grayColor()
+        labelSearch!.textAlignment = NSTextAlignment.Center
+        labelSearch!.font = UIFont(name:"VDS", size:20.0)
+        labelSearch.text = "Searching"
+        labelSearch.backgroundColor = UIColor.whiteColor()
    
         
     }
