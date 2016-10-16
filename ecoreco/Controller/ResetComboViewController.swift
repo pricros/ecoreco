54b08d0ad82878bc7fec99bafd8ecfb1619349a8
Jacky Lin <jacky@huanuage.com>
2016-10-16 20:58:17 +0800

    add reset combo layout

---
+++ ecoreco/Controller/ResetComboViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ResetComboViewController.swift
+//  ecoreco
+//
+//  Created by admin on 2016/10/16.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+class ResetComboViewController: CommonViewController {
+
+    
+}
