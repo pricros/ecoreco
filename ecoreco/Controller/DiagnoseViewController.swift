f14cfd910267c2f9cba14fac41878672a7c6ccdb
Jacky Lin <jacky@huanuage.com>
2016-10-19 04:20:42 +0800

    add diagnose view

---
+++ ecoreco/Controller/DiagnoseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DiagnoseViewController.swift
+//  ecoreco
+//
+//  Created by admin on 2016/10/19.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import Cocoa
+
+class DiagnoseViewController: CommonViewController {
+
+}
