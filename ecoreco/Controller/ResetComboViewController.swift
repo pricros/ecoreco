7d61e0aba234991627990188ccb958574212f4f1
Jacky Lin <jacky@huanuage.com>
2016-10-25 04:58:00 +0800

    add action mapping for reset combo

--- ecoreco/Controller/ResetComboViewController.swift
+++ ecoreco/Controller/ResetComboViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @IBDesignable
-class ResetComboViewController: CommonViewController {
+class ResetComboViewController: CommonViewController, UITextFieldDelegate {
 
     @IBOutlet weak var btnLeft: UIButton!
     
@@ -17,13 +17,35 @@ class ResetComboViewController: CommonViewController {
         
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        txtPassword.delegate = self
+        txtPasswordConfirm.delegate = self
+    }
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtPasswordConfirm: UITextField!
     @IBOutlet weak var btnConfirm: UIButton!
     @IBAction func addLeft(sender: UIButton) {
    
     }
+    @IBAction func addRight(sender: UIButton) {
+    }
     
+    @IBAction func addDown(sender: UIButton) {
+        
+    }
     @IBAction func confirm(sender: UIButton) {
         self.performSegueWithIdentifier("segueRstCmbToSetName", sender: nil)
     }
     
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        txtPassword.resignFirstResponder()
+        txtPasswordConfirm.resignFirstResponder()
+        self.view.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
