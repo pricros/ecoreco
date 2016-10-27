76b94ac7d08a235b750a9d2dff6bc7a7cdbb2a1e
Jacky Lin <jacky@huanuage.com>
2016-10-27 15:44:28 +0800

    cherry-pick ec755f2 1. add new command set 2. refine backgroundThread invoke method

---
+++ ecoreco/Controller/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  ecoreco
+//
+//  Created by admin on 2016/10/27.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: CommonViewController {
+
+
+
+}
