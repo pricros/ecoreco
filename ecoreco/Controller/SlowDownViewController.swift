820ee08b8d458433c0830bf716cbe90ade31358f
Jacky Lin <jacky@huanuage.com>
2016-11-01 00:47:33 +0800

    add slow down view

---
+++ ecoreco/Controller/SlowDownViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SlowDownViewController.swift
+//  ecoreco
+//
+//  Created by apple on 2015/12/22.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+import UIKit
+
+class SlowDownViewController: CommonViewController {
+    
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        // @todo : start a thread to check if the speed < speed limit.
+        
+        // if yes, dismiss this view
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources thatp can be recreated.
+    }
+    
+    
+}
+
