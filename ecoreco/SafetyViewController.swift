0c5a07e8f40c520671845f1e66b686e9dca6fe55
Jacky Lin <jacky@huanuage.com>
2016-11-06 01:05:05 +0800

    1.get phone call from user default 2.setup safety view of settings

--- ecoreco/SafetyViewController.swift
+++ ecoreco/SafetyViewController.swift
@@ -5,15 +5,23 @@
 
 import UIKit
 
-class SafetyViewController: CommonViewController {
+class SafetyViewController: CommonViewController, UITextFieldDelegate {
     
     
     @IBOutlet weak var imgBack: UIImageView!
-    
+    @IBOutlet weak var txtEmergencyCall: UITextField!
+    @IBOutlet weak var txtEmergencySMS: UITextField!
+    let userDefaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txtEmergencyCall.delegate = self
+        txtEmergencyCall.keyboardType = .numberPad
+        
+        
+        txtEmergencySMS.delegate = self
+        txtEmergencySMS.keyboardType = .numberPad
         
         let tapGestureRecognizerImgBack = UITapGestureRecognizer(target: self, action:#selector(RideViewController.tappedBack))
         imgBack.isUserInteractionEnabled = true
@@ -28,6 +36,38 @@ class SafetyViewController: CommonViewController {
     
     func tappedBack(){
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        let checkValue = checkInputValue(inputText: textField.text!)
+        
+        if (checkValue) == true {
+            if (textField)==txtEmergencyCall
+            {
+            //@save data to user default
+                userDefaults.setValue(textField.text, forKey: Constants.kUserDefaultEmergencyCall)
+            }
+            else if (textField)==txtEmergencySMS
+            {
+                userDefaults.setValue(textField.text, forKey: Constants.kUserDefaultEmergencySMS)
+            }
+            
+        } else {
+            
+            
+        }
+        
+        resignFirstResponder()
+        
+        
+        
+        return true
+        
+    }
+    
+    func checkInputValue(inputText:String)->Bool{
+        return true
     }
     
     
